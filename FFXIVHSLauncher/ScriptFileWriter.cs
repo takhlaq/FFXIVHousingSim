fe0cdda38a829d9f7c94fe4dd185b62038a9de30
Tahir Akhlaq <xdemolish@hotmail.com>
2020-07-04 12:39:53 +0100

    add SGActionTransform scripts - todo: different animation types for it, fix scale/rotation things

--- FFXIVHSLauncher/ScriptFileWriter.cs
+++ FFXIVHSLauncher/ScriptFileWriter.cs
@@ -20,7 +20,7 @@ namespace FFXIVHSLauncher
             outStr += "\tfloat delay = " + newDelay + "f;\n";
             outStr += "\tfloat fullRotationTime = " + entry.fullRotationTime + "f;\n";
             outStr += "\tvoid Start(){}\n\n";
-            outStr += "\tvoid Update() {\n";
+            outStr += "\tvoid FixedUpdate() {\n";
 
             outStr += "\t\tif (delay >= 0.0f) { delay -= Time.deltaTime * 10.0f; return; }\n";
             outStr += "\t\ttransform.Rotate";
@@ -52,14 +52,22 @@ namespace FFXIVHSLauncher
             outStr += "\tbyte autoPlay = " + entry.autoPlay + ";\n";
             outStr += "\tfloat delay = " + newDelay + "f;\n";
             outStr += "\tfloat fullRotationTime = " + entry.time + "f;\n";
-            outStr += "\tvoid Start(){}\n\n";
-            outStr += "\tvoid Update() {\n";
+            outStr += "\n\n\tfloat offset = 1.0f;\n";
+            outStr += "\n\n\tfloat timeElapsed = 0.0f;\n";
 
+            outStr += "\tvoid Start(){ }\n\n";
+            outStr += "\tvoid FixedUpdate() {\n";
+
+            outStr += "\t\ttimeElapsed += Time.deltaTime * 10.0f;\n";
             outStr += "\t\tif (autoPlay == 0) return;\n";
             outStr += "\t\tif (delay >= 0.0f) { delay -= Time.deltaTime * 10.0f; return; }\n";
-            outStr += "\t\ttransform.Rotate";
+            outStr += "\t\tif (timeElapsed >= fullRotationTime) { timeElapsed = 0.0f; offset *= -1.0f;  }\n";
+            
+            // todo: fix this, it's wrong
+            // todo: also actually do the horizontal range thing
 
-            string rotateStr = "(float)(100.0f / fullRotationTime)";
+            /*
+            string rotateStr = "\t\ttransform.Rotate(float)(100.0f / fullRotationTime * offset)";
 
             if (entry.rotation == FFXIVHSLib.MapRotationTypeLayer.YAxisOnly)
                 outStr += $"(0.0f, {rotateStr}, 0.0f);";
@@ -67,6 +75,24 @@ namespace FFXIVHSLauncher
                 outStr += $"({rotateStr}, {rotateStr}, {rotateStr});";
             else
                 outStr += "(0.0f, 0.0f, 0.0f);";
+
+            if (entry.swingMoveSpeedRange0 != 0.0f)
+            {
+                if (entry.rotation == FFXIVHSLib.MapRotationTypeLayer.YAxisOnly || entry.rotation == FFXIVHSLib.MapRotationTypeLayer.NoRotate)
+                {
+                    outStr += "\n\t\ttransform.Translate(0.0f, offset * Time.deltaTime, 0.0f);\n";
+                }
+                else if (entry.rotation == FFXIVHSLib.MapRotationTypeLayer.AllAxis)
+                {
+                    outStr += "\n\t\ttransform.Translate(offset * Time.deltaTime, offset * Time.deltaTime, offset * Time.deltaTime);\n";
+                }
+                else
+                {
+
+                }
+
+            }
+            //*/
 
             outStr += "\n\t}";
             outStr += "\n}\n";
@@ -79,10 +105,77 @@ namespace FFXIVHSLauncher
             string outStr = "using System.Collections;\nusing System.Collections.Generic;\nusing UnityEngine;\n\n";
             outStr += "public class " + entry.name + " : MonoBehaviour {\n";
             outStr += "\n\n";
-            outStr += "\tvoid Start(){}\n\n";
-            outStr += "\tvoid Update() {\n";
+            outStr += "\tfloat animTime = " + entry.time + ";\n";
+            outStr += "\tfloat animStartEndTime = " + entry.startEndTime + ";\n";
+            outStr += "\tbool animLoop = " + (entry.loop == 1 ? "true;\n" : "false;\n");
+            outStr += "\tUnityEngine.Vector3 animTranslation = new UnityEngine.Vector3((float)" + entry.translation.x + ", (float)" + entry.translation.y + ", (float)" + entry.translation.z + ");\n";
+            outStr += "\tUnityEngine.Vector3 animRotation = new UnityEngine.Vector3((float)" + entry.rotation.x + ", (float)" + entry.rotation.y + ", (float)" + entry.rotation.z + ");\n";
+            outStr += "\tUnityEngine.Vector3 animScale = new UnityEngine.Vector3((float)" + entry.scale.x + ", (float)" + entry.scale.y + ", (float)" + entry.scale.z + ");\n";
+            outStr += "\tUnityEngine.Vector3 animOffset = new UnityEngine.Vector3((float)" + entry.offset.x + ", (float)" + entry.offset.y + ", (float)" + entry.offset.z + ");\n";
+            outStr += "\t// CurveLinear = 0, CurveSpline = 1;\n";
+            outStr += "\tbyte animCurveType = " + (byte)entry.curveType + ";\n";
+            outStr += "\t// OneWay = 0, RoundTrip = 1, Repetition = 2\n";
+            outStr += "\tbyte animMovementType = " + (byte)entry.movementType + ";\n\n";
 
+            outStr += "\tfloat timeElapsed = 0.0f;\n";
 
+            outStr += "\tvoid Start(){\n";
+            //outStr += "\tinitialPos = Instantiate(transform);\n";
+            outStr +=  "\t}\n";
+            outStr += "\tvoid FixedUpdate() {\n";
+
+            outStr += "timeElapsed += Time.deltaTime * 10.0f;\n";
+            outStr += "if (timeElapsed >= animTime){ timeElapsed = 0.0f; animTranslation *= -1.0f; animRotation *= -1.0f; animScale *= -1.0f; }\n";
+
+            string animStr = "\t\ttransform.Translate(";
+            if (entry.translation.x != 0.0f)
+                animStr += "animTranslation.x * Time.deltaTime,";
+            else
+                animStr += "0.0f,";
+            if (entry.translation.y != 0.0f)
+                animStr += "animTranslation.y * Time.deltaTime,";
+            else
+                animStr += "0.0f,";
+            if (entry.translation.z != 0.0f)
+                animStr += "animTranslation.z);\n";
+            else
+                animStr += "0.0f);\n";
+
+            animStr += "\n\t\ttransform.Rotate(";
+            if (entry.rotation.x != 0.0f)
+                animStr += "animRotation.x * Time.deltaTime,";
+            else
+                animStr += "0.0f,";
+            if (entry.rotation.y != 0.0f)
+                animStr += "animRotation.y * Time.deltaTime,";
+            else
+                animStr += "0.0f,";
+            if (entry.rotation.z != 0.0f)
+                animStr += "animRotation.z * Time.deltaTime);\n";
+            else
+                animStr += "0.0f);\n";
+
+            //*
+            animStr += "\n\t\ttransform.localScale = new UnityEngine.Vector3(transform.localScale.x + ";
+            if (entry.scale.x != 1.0f)
+                animStr += "animScale.x * Time.deltaTime, ";
+            else
+                animStr += "0.0f, ";
+
+            animStr += "transform.localScale.y + ";
+            if (entry.scale.y != 1.0f)
+                animStr += "animScale.y * Time.deltaTime, ";
+            else
+                animStr += "0.0f, ";
+
+            animStr += "transform.localScale.z + ";
+            if (entry.scale.z != 1.0f)
+                animStr += "animScale.z * Time.deltaTime);";
+            else
+                animStr += "0.0f);\n";
+
+            //*/
+            outStr += animStr;
 
             outStr += "\n\t}";
             outStr += "\n}\n";
